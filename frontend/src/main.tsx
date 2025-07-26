54e79c9641d099ae1e2e5d612f6266175af671b8
Potti1234 <lukas.pottner@gmail.com>
2025-07-26 20:18:57 +0200

    Add ethereum sepolia wallet Login with Rainbowkit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,44 @@ import { createRoot } from "react-dom/client";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App.tsx";
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  sepolia
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
+const config = getDefaultConfig({
+  appName: 'PottiSwap',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [sepolia],
+  ssr: false, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConvexProvider client={convex}>
-      <App />
-    </ConvexProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <ConvexProvider client={convex}>
+            <App />
+          </ConvexProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>,
 );
